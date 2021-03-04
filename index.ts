989d92e3e84720ef998a193b5b7ffac09ef84625
BuildTools <is713932@iteso.mx>
2021-03-04 17:40:32 -0600

    Feat: Initial commit and created Chain and Block class

---
+++ index.ts
@@ -0,0 +1,54 @@
+import * as crypto from 'crypto';
+
+
+
+class Block{
+    constructor(
+        public transaction: Transaction,
+        public prevHash: string,
+        public timestamp = Date.now(),
+    ){}
+
+    get hash(){
+        //Stringify the object itself
+        const str = JSON.stringify(this);
+        
+        //Create a hash value with the SHA256 Encryption
+        const hash = crypto.createHash('SHA256');
+        
+        //hash the stringified object
+        hash.update(str).end();
+
+        //return as a hexadecimal string
+        return hash.digest('hex');
+
+    }
+}
+
+class Chain{
+    //there can only be one chain; Singleton instance
+    public static instance = new Chain();
+
+    chain: Block[];
+
+    constructor(){
+        //genesis block
+        this.chain = [new Block(new Transaction(),'')
+        ];
+    }
+
+    get lastBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+}
+
+class Transaction{
+    constructor() {}
+    
+      
+}
+
+class Wallet {
+    constructor() {}
+
+}
